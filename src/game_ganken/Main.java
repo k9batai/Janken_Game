4515aef24e326273f4a88c7601e934090f6c5fca
k9 <k9batai33@gmail.com>
2022-09-21 18:51:22 +0900

    First commit

---
+++ src/game_ganken/Main.java
@@ -0,0 +1,22 @@
+package game_ganken;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//JFrameクラスをインスタンス化
+		JFrame frame = new JFrame("じゃんけんゲーム");
+		//ウィンドウのサイズを指定
+		frame.setSize(640, 480);
+		//ウィンドウの位置を、画面の中心へ
+		frame.setLocationRelativeTo(null);
+		//×でアプリが終了するようにする
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//ウィンドウのサイズを変更できないようにする
+		frame.setResizable(false);
+		//ウィンドウを表示する
+		frame.setVisible(true);
+	}
+
+}
